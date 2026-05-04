2a392b175dbd20e976342ac58745e724392e7535
Thomas Paula <tsp.thomas@gmail.com>
2026-05-03 21:00:39 -0300

    Update scripts to compile results

--- scripts/analyze_stability.sh
+++ scripts/analyze_stability.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
-experiment_name="qwen35_08b_cl"
+#experiment_name="qwen35_08b_cl_paper"
+#experiment_name="qwen35_08b_paper_321"
+#experiment_name="llama32_1b_cl_paper"
+#experiment_name="llama32_1b_cl_paper_321"
+#experiment_name="gemma3_1b_cl_paper"
+experiment_name="gemma3_1b_cl_paper_321"
 experiment_output_dir="results/${experiment_name}/stability_metrics"
 
 uv run scripts/analyze_stability.py \
--- scripts/compute_metrics.sh
+++ scripts/compute_metrics.sh
@@ -1,8 +1,14 @@
 #/bin/bash
 
-experiment_name="qwen35_08b_cl"
+#experiment_name="qwen35_08b_cl_paper"
+#experiment_name="qwen35_08b_paper_321"
+#experiment_name="llama32_1b_cl_paper"
+#experiment_name="llama32_1b_cl_paper_321"
+#experiment_name="gemma3_1b_cl_paper"
+experiment_name="gemma3_1b_cl_paper_321"
 experiment_results_dir="results/${experiment_name}/cl_metrics"
 
 uv run scripts/compute_metrics.py \
   --scores-csv ${experiment_results_dir}/scores_all.csv \
-  --task-order task_1,task_2,task_3
+  --task-order task_3,task_2,task_1
+  #--task-order task_1,task_2,task_3
