68ec59a0203916c1fc7bda8f4950e1dc0816d110
Thomas Paula <tsp.thomas@gmail.com>
2026-09-02 08:29:05 -0300

    Improve scripts to allow multiple experiments to be evaluated

--- scripts/analyze_stability.sh
+++ scripts/analyze_stability.sh
@@ -1,13 +1,20 @@
 #/bin/bash
 
-#experiment_name="qwen35_08b_cl_paper"
-#experiment_name="qwen35_08b_paper_321"
-#experiment_name="llama32_1b_cl_paper"
-#experiment_name="llama32_1b_cl_paper_321"
-#experiment_name="gemma3_1b_cl_paper"
-experiment_name="gemma3_1b_cl_paper_321"
-experiment_output_dir="results/${experiment_name}/stability_metrics"
+experiments=(
+  "qwen35_08b_paper"
+  "qwen35_08b_paper_321"
+  "llama32_1b_paper"
+  "llama32_1b_paper_321"
+  "gemma3_1b_paper"
+  "gemma3_1b_paper_321"
+)
 
-uv run scripts/analyze_stability.py \
-  --input outputs/${experiment_name} \
-  --output-dir ${experiment_output_dir}
+echo -e "\033[36mAnalyzing stability\033[0m"
+for experiment_name in "${experiments[@]}"; do
+  echo -e "\033[36mProcessing: ${experiment_name}\033[0m"
+  experiment_output_dir="results/${experiment_name}/stability_metrics"
+
+  uv run scripts/analyze_stability.py \
+    --input outputs/${experiment_name} \
+    --output-dir ${experiment_output_dir}
+done
--- scripts/combine_scores.sh
+++ scripts/combine_scores.sh
@@ -1,13 +1,19 @@
 #/bin/bash
 
-#experiment_name="qwen35_08b_cl_paper"
-#experiment_name="qwen35_08b_paper_321"
-#experiment_name="llama32_1b_cl_paper"
-#experiment_name="llama32_1b_cl_paper_321"
-#experiment_name="gemma3_1b_cl_paper"
-experiment_name="gemma3_1b_cl_paper_321"
-experiment_output_dir="results/${experiment_name}/cl_metrics"
+experiments=(
+  "qwen35_08b_paper"
+  "qwen35_08b_paper_321"
+  "llama32_1b_paper"
+  "llama32_1b_paper_321"
+  "gemma3_1b_paper"
+  "gemma3_1b_paper_321"
+)
 
-uv run scripts/combine_scores.py \
-  --run-dir outputs/${experiment_name} \
-  --output-dir ${experiment_output_dir}
+for experiment_name in "${experiments[@]}"; do
+  echo -e "\033[36mProcessing: ${experiment_name}\033[0m"
+  experiment_output_dir="results/${experiment_name}/cl_metrics"
+
+  uv run scripts/combine_scores.py \
+    --run-dir outputs/${experiment_name} \
+    --output-dir ${experiment_output_dir}
+done
